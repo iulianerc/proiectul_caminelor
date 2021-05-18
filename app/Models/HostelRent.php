5bcac9c9470845d674726da72f1d8f4f9a2dfef1
iulianerc <colesnic.2002@mail.ru>
2021-05-19 00:57:24 +0300

    Perform

--- app/Models/HostelRent.php
+++ app/Models/HostelRent.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Hostels extends Model
+class HostelRent extends Model
 {
     use HasFactory;
 }
---
+++ database/seeds/com/HostelRentsSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders\com;
+
+use Illuminate\Database\Seeder;
+
+class HostelRentsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
